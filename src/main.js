3e75ae5d9cfb3b236d67f5a19afbe9a6701b1505
Badtz <badtzmaru139@gmail.com>
2021-03-28 16:31:12 -0700

    set up firebase in app

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
+import firebase from 'firebase/app';
 import Vue from 'vue';
 import App from './App.vue';
+import config from './firebaseConfig';
 import './index.css';
 import router from './router';
+
+firebase.initializeApp(config);
 
 Vue.config.productionTip = false;
 
