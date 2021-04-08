0e9f8cc447ca435eece6e5d040a29b6c6fd5cccb
Badtz <badtzmaru139@gmail.com>
2021-04-08 15:49:50 -0700

    Update Feed.vue

--- src/components/Feed.vue
+++ src/components/Feed.vue
@@ -1,15 +1,27 @@
 <template>
   <div class="bg-indigo-500 h-full w-full">
     <StatusTile />
+    {{uid}}
   </div>
 </template>
 
 <script>
 
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
 import StatusTile from './StatusTile.vue';
 
 export default {
   name: 'Feed',
+  props: {
+    uid: String,
+  },
+  data() {
+    return {
+      friendData: {},
+    };
+  },
   components: {
     StatusTile,
   },
